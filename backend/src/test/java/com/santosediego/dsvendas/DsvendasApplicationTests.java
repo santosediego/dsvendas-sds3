42d069b3b5571478d4fec42dd3c567ff8c401a08
santosediego <diegoeustaquiodossantos@hotmail.com>
2021-05-03 13:05:16 -0300

    Created projects

---
+++ backend/src/test/java/com/santosediego/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosediego.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
